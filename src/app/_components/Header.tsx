e2d8b84cb3bb99d87b1e018004cc73fff238516c
BoatengPeter <boatengkonadupeter@gmail.com>
2024-08-20 04:51:37 -1200

    .

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -2,12 +2,14 @@ import { Menu } from 'lucide-react'
 import { SignInButton, SignedOut, SignedIn, UserButton } from "@clerk/nextjs"
 import { Button } from "~/components/ui/button";
 
+
 const Header = ({ children }: { children?: React.ReactNode }) => {
     return (
-        <header className="flex justify-between items-center mt-4 mb-16 ">
+        <header className="flex justify-between items-center mt-2 md:mt-4 mb-12 md:mb-16 ">
             <h1 className="text-3xl font-bold text-black">DOiT</h1>
-            <Menu size={24} strokeWidth={2} className="md:hidden text-black" />
-            <div className="hidden md:flex flex-row gap-4">
+
+
+            <div className=" flex flex-row gap-2 md:gap-4">
                 <SignedIn>
                     <UserButton />
                 </SignedIn>
